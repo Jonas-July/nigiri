7be720c220b0596b03ed2f3170c5bd4b05f354f5
Jonas-July <jonas.july@outlook.de>
2025-09-23 12:55:40 +0200

    refactor: Group bitfield index calculation together

--- src/loader/load.cc
+++ src/loader/load.cc
@@ -176,12 +176,6 @@ timetable load(std::vector<timetable_source> const& sources,
       auto const progress_tracker = utl::get_active_progress_tracker();
       progress_tracker->context(std::string{tag});
       progress_tracker->status("Merging...");
-      auto bitfields_ = hash_map<bitfield, bitfield_idx_t>{};
-      for (auto const [idx_, bf] : utl::enumerate(tt.bitfields_)) {
-        auto new_idx = utl::get_or_create(
-          bitfields_, bf, [&]() { return idx_; });
-        assert(new_idx == idx_); // bitfields must be unique in the timetable
-      }
       /* Prepare timetable by emptying corrected fields */
       // Fields not used during loading
       assert(tt.locations_.footpaths_out_.size() == kNProfiles);
@@ -284,6 +278,12 @@ timetable load(std::vector<timetable_source> const& sources,
       /* Add new data and adjust references */
       /*	bitfields	*/
       auto corrected_indices = vector_map<bitfield_idx_t, bitfield_idx_t>{};
+      auto bitfields_ = hash_map<bitfield, bitfield_idx_t>{};
+      for (auto const [idx_, bf] : utl::enumerate(tt.bitfields_)) {
+        auto new_idx = utl::get_or_create(
+          bitfields_, bf, [&]() { return idx_; });
+        assert(new_idx == idx_); // bitfields must be unique in the timetable
+      }
       for (auto const& [idx_, bf] : utl::enumerate(new_bitfields)) {
         auto adjusted_idx = utl::get_or_create(
             bitfields_, bf, [&]() { return tt.register_bitfield(bf); });
